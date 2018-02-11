08e62f2711dc39070cd072ab5187e3fc34e7ba9f
Erehuc <huc.romain@gmail.com>
2018-02-11 20:30:46 +0100

    http

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
 	else {
 		next();
 	}
-};)
+});
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
